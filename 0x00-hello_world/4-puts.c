c0ad3e2cadd187597a63ccff493963d78d4046e0
Khalil Sangare <khalilsang@gmail.com>
2022-09-08 16:00:14 -0700

    Add 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-/*
- *main : main function; Entry
- *
- *Return: 0
- *
+/**
+
+ * main - Entry point
+
+ * 
+
+ * Description: 'a program to print a message'
+
+ * 
+
+ * Return: Always 0 (Success)
+
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
