765e08548fcae946123c5e1f7239c7b8830fcecc
Khalil Sangare <khalilsang@gmail.com>
2022-09-09 19:53:56 -0700

    Add 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)  
+int main(void)
 {
-  int d;
-  for (d = '0'; d <= '9'; d++)
-    {
-    putchar(d);  
-    if (d != '9')
-      {
-	putchar(',');
-	putchar(' ');
-      }
-    }
-  putchar('\n');
-  return (0);
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);  
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
