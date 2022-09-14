214018ade83ca5bafbe72d8d1b14991e7d56e5dc
Khalil <khalilsang@gmail.com>
2022-09-14 22:52:34 +0000

    update 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main : Print alphabet 10 times
+ *
+ * Return : 0 for success
+ *
+ */
+int main (void)
+{
+  int i, j;
+  for (i = 0; i < 9; i++)
+    {
+      for(j = 'a'; j <= 'z'; j++)
+	{
+	  _putchar(j);
+	}
+      _putchar('\n');
+      return(0);
+    }
+}
