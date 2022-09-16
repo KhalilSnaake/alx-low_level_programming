ed744a903c3af5899ff76c2aff3d53566685f60a
Khalil <khalilsang@gmail.com>
2022-09-16 22:56:28 +0000

    New project 0x04-more_functions_nested_loops directory, README.md and 0-isupper.c files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - check if letter is uppercase
+ *
+ *@c : parameter int c
+ *Return : 0 if not 1 if upper
+ *
+ */
+int main( int c)
+{
+  if (isupper (c))
+    {
+      return (0);
+    }
+  else
+    {
+      return (0);
+    }
+}
