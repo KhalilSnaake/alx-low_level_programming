d55576c9af46e9e6680367f4be9b2971a927263f
= <=>
2022-10-10 21:22:34 +0000

    New directory 0x0E-structures_typedef inside README.md file, dog.h file, 1-init_dog.c file, 2-print_dog.c file, 4-new_dog.c file and 5-free_dog.c file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,10 @@
+# 0x0E. C - Structures, typedef
+
+## Tasks
+
+### 0. Poppy
+### dog.h 
+### 1-init_dog.c
+### 2-print_dog.c
+### 4-new_dog.c
+### 5-free_dog.c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * doggy - Typedef for struct dog
+ */
+typedef struct dog doggy;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+doggy *new_dog(char *name, float age, char *owner);
+void free_dog(doggy *d);
+
+#endif
